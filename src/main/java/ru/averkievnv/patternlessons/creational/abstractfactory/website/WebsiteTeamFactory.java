4882ea726a93a83cfa053d07dba525bdab7877c6
Nikolay <averkievnv@gmail.com>
2023-07-12 21:10:20 +0300

    Update WebsiteTeamFactory.java

--- src/main/java/ru/averkievnv/patternlessons/creational/abstractfactory/website/WebsiteTeamFactory.java
+++ src/main/java/ru/averkievnv/patternlessons/creational/abstractfactory/website/WebsiteTeamFactory.java
@@ -10,12 +10,10 @@ public class WebsiteTeamFactory implements ProjectTeamFactory {
     public Developer getDeveloper() {
         return new PhpDeveloper();
     }
-
     @Override
     public Tester getTester() {
         return new ManualTester();
     }
-
     @Override
     public ProjectManager getProjectManager() {
         return new WebsitePM();
